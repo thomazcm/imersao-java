d20b6c8748f756ebd557c5a3227b650f9423763d
thomazcm <71472870+thomazcm@users.noreply.github.com>
2022-07-21 13:59:26 -0300

    Projeto fim do primeiro vídeo
    Projeto java do fim do primeiro vídeo.
    Mudanças na classe JsonParser pra corrigir uma falha no RegEx que trazia os nomes de algumas categorias precedidos de um ", 
    Adição de um check pra se fará realmente uma nova conexão com API ou usará o string armazenado em otura classe, para evitar atingir o limite de acessos da API durante testes.

---
+++ stickersimersao/App.java
@@ -0,0 +1,44 @@
+package br.com.alura.stickersimersao;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+import br.com.alura.stickersimersao.util.JsonParser;
+import br.com.alura.stickersimersao.util.RequestSaver;
+
+public class App {
+    public static void main(String[] args) {
+        String json = pegarJsonString("https://imdb-api.com/en/API/Top250Movies/k_l3uzs1py", true);
+        List<Map<String, String>> listaDeFilmes = new JsonParser().parse(json);
+
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+
+    private static String pegarJsonString(String url, Boolean newRequest) {
+        URI endereco = URI.create(url);
+        var httpClient = HttpClient.newHttpClient();
+        var httpRequest = HttpRequest.newBuilder(endereco).GET().build();
+
+        if (newRequest) {
+            try {
+                HttpResponse<String> response = httpClient.send(httpRequest, BodyHandlers.ofString());
+                return response.body();
+            } catch (IOException | InterruptedException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        return RequestSaver.getJson();
+    }
+}
